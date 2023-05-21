25b2ab3829a9feaf06c01187da8660827e9af1e2
Bitlet <j.mensink0206@gmail.com>
2023-05-21 10:50:20 +0200

    Commit 1.0

--- referencable/js/Skinport_UI_Framework.js
+++ referencable/js/Skinport_UI_Framework.js
@@ -1,30 +1,40 @@
-// ==UserScript==
-// @name         Skinport UI Framework
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  A Skinport Utility Plugin
-// @author       Jay
-// @match        https://skinport.com/*
-// @icon         https://jaymensink.github.io/images/watching.png
-// ==/UserScript==
-
-
-
-// TODO
-// Implement delete methods of elements removing them from FilterPanel.elements
-// Finish writing DropDown class
+/////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                     //
+//      ██████╗██╗  ██╗██╗███╗  ██╗██████╗  █████╗ ██████╗ ████████╗  ██╗   ██╗██╗     //
+//     ██╔════╝██║ ██╔╝██║████╗ ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝  ██║   ██║██║     //
+//     ╚█████╗ █████═╝ ██║██╔██╗██║██████╔╝██║  ██║██████╔╝   ██║     ██║   ██║██║     //
+//      ╚═══██╗██╔═██╗ ██║██║╚████║██╔═══╝ ██║  ██║██╔══██╗   ██║     ██║   ██║██║     //
+//     ██████╔╝██║ ╚██╗██║██║ ╚███║██║     ╚█████╔╝██║  ██║   ██║     ╚██████╔╝██║     //
+//     ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚══╝╚═╝      ╚════╝ ╚═╝  ╚═╝   ╚═╝      ╚═════╝ ╚═╝     //
+//                                                                                     //
+//  ███████╗██████╗  █████╗ ███╗   ███╗███████╗ ██╗       ██╗ █████╗ ██████╗ ██╗  ██╗  //
+//  ██╔════╝██╔══██╗██╔══██╗████╗ ████║██╔════╝ ██║  ██╗  ██║██╔══██╗██╔══██╗██║ ██╔╝  //
+//  █████╗  ██████╔╝███████║██╔████╔██║█████╗   ╚██╗████╗██╔╝██║  ██║██████╔╝█████═╝   //
+//  ██╔══╝  ██╔══██╗██╔══██║██║╚██╔╝██║██╔══╝    ████╔═████║ ██║  ██║██╔══██╗██╔═██╗   //
+//  ██║     ██║  ██║██║  ██║██║ ╚═╝ ██║███████╗  ╚██╔╝ ╚██╔╝ ╚█████╔╝██║  ██║██║ ╚██╗  //
+//  ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝   ╚═╝   ╚═╝   ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝  //
+//                                                                                     //
+//                        Written and Maintained by Jay Mensink                        //
+//                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
 
 class FilterPanel {
 
-	constructor(name, opened=false, onOpen=function(){}, onClose=function(){}, onFlip=function(){}) {
+	constructor(name, opened=false, hidden=false, onOpen=function(){}, onClose=function(){}, onFlip=function(){}, onAddElement=function(){}, onRename=function(){}, onShow=function(){}, onHide=function(){}, onToggleVisibility=function(){}) {
 
 		this.name = name;
 		this.opened = opened;
+		this.hidden = hidden;
 		this.onOpen = onOpen;
 		this.onClose = onClose;
 		this.onFlip = onFlip;
+		this.onAddElement = onAddElement;
+		this.onRename = onRename;
+		this.onShow = onShow;
+		this.onHide = onHide;
+		this.onToggleVisibility = onToggleVisibility;
 
 		this.elements = [];
 
@@ -34,15 +44,36 @@ class FilterPanel {
 		this.element.querySelector(".FilterWrapper-header").addEventListener("click", this.flip )
 		
 		if (opened) { this.open(); }
+		if (hidden) { this.hide(); }
 
 		document.querySelector(".CatalogFilter").firstElementChild.before(this.element);
 
 	}
 
-	setName = (name) => {
+	rename = (name) => {
 		this.name = name;
 		this.element.querySelector(".FilterWrapper-title").innerText = name;
 		this.element.querySelector(".FilterWrapper-content").firstElementChild.className = name.trim().replace(" ", "");
+		this.onRename();
+	}
+
+	toggleVisibility = () => {
+		this.hidden = !this.hidden;
+		if (this.hidden) { this.hide(); }
+		else { this.show(); }
+		this.onToggleVisibility();
+	}
+
+	show = () => {
+		this.hidden = false;
+		this.element.style.display = "";
+		this.onShow();
+	}
+
+	hide = () => {
+		this.hidden = true;
+		this.element.style.display = "none";
+		this.onHide();
 	}
 
 	flip = () => {
@@ -72,28 +103,36 @@ class FilterPanel {
 		let checkbox = new Checkbox(name, enabled, hidden, onActivate, onDeactivate, onToggle, onShow, onHide, onToggleVisibility, onRename);
 		this.elements.push( checkbox );
 		this.element.querySelector(".FilterWrapper-content").firstElementChild.appendChild( checkbox.element );
+		this.onAddElement();
+		return checkbox;
 	}
 
-	addTextbox = (name, prefix="", placeholder="...", value="", onInput=function(){}) => {
-		let textbox = new Textbox(name, prefix, placeholder, value, onInput);
+	addTextbox = (name, prefix="", placeholder="...", value="", hidden=false, onInput=function(){}, onShow=function(){}, onHide=function(){}, onToggleVisibility=function(){}, onRename=function(){}, onSetPrefix=function(){}, onSetPlaceholder=function(){}, onSetValue=function(){}) => {
+		let textbox = new Textbox(name, prefix, placeholder, value, hidden, onInput, onShow, onHide, onToggleVisibility, onRename, onSetPrefix, onSetPlaceholder, onSetValue);
 		this.elements.push( textbox );
 		this.element.querySelector(".FilterWrapper-content").firstElementChild.appendChild( textbox.element );
+		this.onAddElement();
+		return textbox;
 	}
 
-	addButton = (name, onClick=function(){}) => {
-		let button = new Button(name, onClick);
+	addButton = (name, hidden=false, onClick=function(){}, onRename=function(){}, onShow=function(){}, onHide=function(){}, onToggleVisibility=function(){}) => {
+		let button = new Button(name, hidden, onClick, onRename, onShow, onHide, onToggleVisibility);
 		this.elements.push( button );
 		this.element.querySelector(".FilterWrapper-content").firstElementChild.appendChild( button.element );
+		this.onAddElement();
+		return button;
+	}
+
+	addDropdown = (name, hidden=false, onOpen=function(){}, onClose=function(){}, onFlip=function(){}, onSelect=function(){}, onShow=function(){}, onHide=function(){}, onToggleVisibility=function(){}, onRename=function(){}) => {
+		let dropdown = new Dropdown(name, hidden, onOpen, onClose, onFlip, onSelect, onShow, onHide, onToggleVisibility, onRename);
+		this.elements.push( dropdown );
+		this.element.querySelector(".FilterWrapper-content").firstElementChild.appendChild( dropdown.element );
+		this.onAddElement();
+		return dropdown;
 	}
 
 	toString = () => {
 		return this.title;
-	}
-
-	delete = () => {
-		for (const element of this.elements) { element.delete(); }
-		this.elements = [];
-		this.element.parentElement.removeChild(this.element);
 	}
 
 }
@@ -120,7 +159,7 @@ class Checkbox {
 		this.element.setAttribute("role", "presentation");
 		this.element.innerHTML = "<div class=\"Checkbox Checkbox--center\"><input class=\"Checkbox-input\" type=\"checkbox\"><div class=\"Checkbox-overlay\"></div><div class=\"Checkbox-label\"></div></div>";
 		
-		this.setName(name);
+		this.element.querySelector(".Checkbox-label").innerText = name;
 		if (enabled) { this.activate(); }
 		if (hidden) { this.hide(); }
 		
@@ -128,7 +167,7 @@ class Checkbox {
 
 	}
 
-	setName = (name) => {
+	rename = (name) => {
 		this.name = name;
 		this.element.querySelector(".Checkbox-label").innerText = name;
 		this.onRename();
@@ -176,23 +215,27 @@ class Checkbox {
 		return this.name;
 	}
 
-	delete = () => {
-		this.element.parentElement.removeChild(this.element);
-	}
-
 }
 
 
 
 class Textbox {
 
-	constructor(name, prefix, placeholder, value, onInput) {
+	constructor(name, prefix, placeholder, value, hidden, onInput, onShow, onHide, onToggleVisibility, onRename, onSetPrefix, onSetPlaceholder, onSetValue) {
 
 		this.name = name;
 		this.prefix = prefix;
 		this.placeholder = placeholder;
 		this.value = value;
+		this.hidden = hidden;
 		this.onInput = onInput;
+		this.onShow = onShow;
+		this.onHide = onHide;
+		this.onToggleVisibility = onToggleVisibility;
+		this.onRename = onRename;
+		this.onSetPrefix = onSetPrefix;
+		this.onSetPlaceholder = onSetPlaceholder;
+		this.onSetValue = onSetValue;
 
 		this.element = document.createElement("label");
 		this.element.classList.add("PriceFilter-inputField");
@@ -208,37 +251,58 @@ class Textbox {
 
 		this.element.querySelector("input").value = value;
 
+		if (hidden) { this.hide(); }
+
 		this.element.querySelector("input").addEventListener("input", (event) => { this.value = event.target.value; } );
 		this.element.querySelector("input").addEventListener("input", onInput);
 
 	}
 
-	setName = (name) => {
+	rename = (name) => {
 		this.name = name;
 		this.element.querySelector(".PriceFilter-label").innerText = name;
+		this.onRename();
 	}
 
 	setPrefix = (prefix) => {
 		this.prefix = prefix;
 		if ( this.element.querySelectorAll(".PriceFilter-currency").length != 0 ) { this.element.querySelector(".PriceFilter-currency").innerText = prefix; }
+		this.onSetPrefix();
 	}
 
 	setPlaceholder = (placeholder) => {
 		this.placeholder = placeholder;
 		this.element.querySelector(".PriceFilter-input").setAttribute("placeholder", placeholder);
+		this.onSetPlaceholder();
 	}
 
 	setValue = (value) => {
 		this.value = value;
 		this.element.querySelector("input").value = value;
+		this.onSetValue();
+	}
+
+	toggleVisibility = () => {
+		this.hidden = !this.hidden;
+		if (this.hidden) { this.hide(); }
+		else { this.show(); }
+		this.onToggleVisibility();
+	}
+
+	show = () => {
+		this.hidden = false;
+		this.element.style.display = "";
+		this.onShow();
+	}
+
+	hide = () => {
+		this.hidden = true;
+		this.element.style.display = "none";
+		this.onHide();
 	}
 
 	toString = () => {
 		return this.name + ": " + this.value;
-	}
-
-	delete = () => {
-		this.element.parentElement.removeChild(this.element);
 	}
 
 }
@@ -247,10 +311,15 @@ class Textbox {
 
 class Button {
 
-	constructor(name, onClick) {
+	constructor(name, hidden, onClick, onRename, onShow, onHide, onToggleVisibility) {
 
 		this.name = name;
+		this.hidden = hidden;
 		this.onClick = onClick;
+		this.onRename = onRename;
+		this.onShow = onShow;
+		this.onHide = onHide;
+		this.onToggleVisibility = onToggleVisibility;
 
 		this.element = document.createElement("button");
 		this.element.classList.add("SubmitButton");
@@ -259,31 +328,62 @@ class Button {
 		this.element.style.marginTop = "8px";
 		this.element.style.width = "80%";
 		this.element.style.marginLeft = "30px";
-		this.rename(name);
+		this.element.innerHTML = "<div class=\"SubmitButton-title\">" + name + "</div>";
 		this.element.addEventListener("click", onClick);
+
+		if (hidden) { this.hide(); }
 
 	}
 
 	rename = (name) => {
 		this.name = name;
 		this.element.innerHTML = "<div class=\"SubmitButton-title\">" + name + "</div>";
+		this.onRename();
+	}
+
+	toggleVisibility = () => {
+		this.hidden = !this.hidden;
+		if (this.hidden) { this.hide(); }
+		else { this.show(); }
+		this.onToggleVisibility();
+	}
+
+	show = () => {
+		this.hidden = false;
+		this.element.style.display = "";
+		this.onShow();
+	}
+
+	hide = () => {
+		this.hidden = true;
+		this.element.style.display = "none";
+		this.onHide();
 	}
 
 	toString = () => {
 		return this.name;
 	}
 
-	delete = () => {
-		this.element.parentElement.removeChild(this.element);
-	}
-
 }
+
 
 
 class Dropdown {
 
-	constructor() {
+	constructor(name, hidden, onOpen, onClose, onFlip, onSelect, onShow, onHide, onToggleVisibility, onRename) {
 
+		this.name = name;
+		this.hidden = hidden;
+		this.onOpen = onOpen;
+		this.onClose = onClose;
+		this.onFlip = onFlip
+		this.onSelect = onSelect;
+		this.onShow = onShow;
+		this.onHide = onHide;
+		this.onToggleVisibility = onToggleVisibility;
+		this.onRename = onRename;
+		this.opened = false;
+		this.selected = null;
 		this.options = [];
 
 		this.element = document.createElement("div");
@@ -292,30 +392,83 @@ class Dropdown {
 		this.element.style.width = "80%";
 		this.element.style.marginLeft = "30px";
 		this.element.style.marginBottom = "8px";
-		this.element.innerHTML = "<label class=\"adyen-checkout__label\"><div class=\"adyen-checkout__input-wrapper\"><div class=\"adyen-checkout__dropdown Select-module_adyen-checkout__dropdown__0Mj-n adyen-checkout__issuer-list__dropdown\"><div aria-disabled=\"false\" aria-expanded=\"false\" aria-haspopup=\"listbox\" class=\"adyen-checkout__dropdown__button Select-module_adyen-checkout__dropdown__button__yTyqq\" role=\"button\" tabindex=\"0\" title=\"Select your bank\" id=\"\"><span class=\"adyen-checkout__dropdown__button__text\">Select your bank</span></div><ul class=\"test adyen-checkout__dropdown__list Select-module_adyen-checkout__dropdown__list__YtEzj\" id=\"select-9f75bc30-152a-4394-a9e4-96e86f1a7d1c\" role=\"listbox\"></ul></div></div></label>";
+		this.element.innerHTML = "<label class=\"adyen-checkout__label\"><div class=\"adyen-checkout__input-wrapper\"><div class=\"adyen-checkout__dropdown Select-module_adyen-checkout__dropdown__0Mj-n adyen-checkout__issuer-list__dropdown\"><div aria-disabled=\"false\" aria-expanded=\"false\" aria-haspopup=\"listbox\" class=\"adyen-checkout__dropdown__button Select-module_adyen-checkout__dropdown__button__yTyqq\" role=\"button\" tabindex=\"0\" title=\"" + name + "\" id=\"\"><span class=\"adyen-checkout__dropdown__button__text\">" + name + "</span></div><ul class=\"test adyen-checkout__dropdown__list Select-module_adyen-checkout__dropdown__list__YtEzj\" id=\"select-9f75bc30-152a-4394-a9e4-96e86f1a7d1c\" role=\"listbox\"></ul></div></div></label>";
 		
-		this.element.querySelector(".adyen-checkout__dropdown__button").addEventListener("click", function() {
-			if ( this.element.querySelector(".adyen-checkout__dropdown__list").classList.contains("adyen-checkout__dropdown__list--active") ) {
-				this.element.querySelector(".adyen-checkout__dropdown__list").classList.remove("adyen-checkout__dropdown__list--active");
-				this.element.querySelector(".adyen-checkout__dropdown__list").classList.remove("Select-module_adyen-checkout__dropdown__list--active__Gegw2");
-			} else {
-				this.element.querySelector(".adyen-checkout__dropdown__list").classList.add("adyen-checkout__dropdown__list--active");
-				this.element.querySelector(".adyen-checkout__dropdown__list").classList.add("Select-module_adyen-checkout__dropdown__list--active__Gegw2");
-			}
-		})
-		this.element.querySelectorAll("li").forEach(function(option) {
-			option.addEventListener("click", function() {
-				this.element.querySelector(".adyen-checkout__dropdown__button").innerHTML = "<img class=\"adyen-checkout__dropdown__button__icon adyen-checkout__image adyen-checkout__image--loaded\" src=\"" + option.querySelector("img").src + "\" alt=\"ING\"><span class=\"adyen-checkout__dropdown__button__text\">" + option.querySelector("span").innerText + "</span>"
-				this.element.querySelector(".adyen-checkout__dropdown__list").classList.remove("adyen-checkout__dropdown__list--active");
-				this.element.querySelector(".adyen-checkout__dropdown__list").classList.remove("Select-module_adyen-checkout__dropdown__list--active__Gegw2");
-			})
-		})
+		this.element.querySelector(".adyen-checkout__dropdown__button").addEventListener("click", this.flip )
 
+		this.element.querySelectorAll("li").forEach((option) => { option.addEventListener("click", () => { this.select(option) }); })
+
+		if (hidden) { this.hide(); }
+
+	}
+
+	rename = (name) => {
+		this.element.querySelector(".Select-module_adyen-checkout__dropdown__button__yTyqq").title = name;
+		this.element.querySelector(".adyen-checkout__dropdown__button__text").innerText = name;
+		this.onRename();
+	}
+	
+	toggleVisibility = () => {
+		this.hidden = !this.hidden;
+		if (this.hidden) { this.hide(); }
+		else { this.show(); }
+		this.onToggleVisibility();
+	}
+
+	show = () => {
+		this.hidden = false;
+		this.element.style.display = "";
+		this.onShow();
+	}
+
+	hide = () => {
+		this.hidden = true;
+		this.element.style.display = "none";
+		this.onHide();
+	}
+
+	select = (option) => {
+		option = option.closest("li");
+		this.element.querySelector(".adyen-checkout__dropdown__button").innerHTML = "<img class=\"adyen-checkout__dropdown__button__icon adyen-checkout__image adyen-checkout__image--loaded\" src=\"" + option.querySelector("img").src + "\" alt=\"ING\"><span class=\"adyen-checkout__dropdown__button__text\">" + option.querySelector("span").innerText + "</span>"
+		this.selected = option.querySelector("span").innerText;
+		this.close();
+		this.onSelect();
+	}
+
+	flip = () => {
+		this.opened = !this.opened;
+		if (this.opened) { this.open(); }
+		else { this.close(); }
+		this.onFlip();
+	}
+
+	open = () => {
+		this.opened = true;
+		this.element.querySelector(".adyen-checkout__dropdown__list").classList.add("adyen-checkout__dropdown__list--active");
+		this.element.querySelector(".adyen-checkout__dropdown__list").classList.add("Select-module_adyen-checkout__dropdown__list--active__Gegw2");
+		this.onOpen();
+	}
+
+	close = () => {
+		this.opened = false;
+		this.element.querySelector(".adyen-checkout__dropdown__list").classList.remove("adyen-checkout__dropdown__list--active");
+		this.element.querySelector(".adyen-checkout__dropdown__list").classList.remove("Select-module_adyen-checkout__dropdown__list--active__Gegw2");
+		this.onClose();
 	}
 
 	addOption = (option) => {
 		this.options.push(option);
-		this.element.querySelector("ul").innerHTML += "<li aria-disabled=\"false\" aria-selected=\"false\" class=\"adyen-checkout__dropdown__element Select-module_adyen-checkout__dropdown__element__ORU4-\" data-value=\"0721\" role=\"option\" tabindex=\"-1\"><img class=\"adyen-checkout__dropdown__element__icon adyen-checkout__image adyen-checkout__image--loaded\" alt=\"ING\" src=\"" + option.image + "\"><span class=\"adyen-checkout__dropdown__element__text\">" + option.name + "</span></li>";
+		let optionElement = document.createElement("li");
+		optionElement.setAttribute("aria-disabled", false);
+		optionElement.setAttribute("aria-selected", false);
+		optionElement.classList.add("adyen-checkout__dropdown__element");
+		optionElement.classList.add("Select-module_adyen-checkout__dropdown__element__ORU4-");
+		optionElement.setAttribute("data-value", "0721");
+		optionElement.setAttribute("role", "option");
+		optionElement.setAttribute("tabindex", "-1");
+		optionElement.innerHTML = "<img class=\"adyen-checkout__dropdown__element__icon adyen-checkout__image adyen-checkout__image--loaded\" alt=\"" + option.name + "\" src=\"" + option.image + "\"><span class=\"adyen-checkout__dropdown__element__text\">" + option.name + "</span>";
+		this.element.querySelector("ul").appendChild(optionElement);
+		optionElement.addEventListener("click", () => { this.select(optionElement); });
 	}
 
 }
@@ -332,3 +485,22 @@ class DropdownOption {
 	}
 
 }
+
+
+
+
+
+// Test values
+let panel = new FilterPanel("Example Panel");
+
+let checkbox = panel.addCheckbox("Example Checkbox");
+
+let textbox = panel.addTextbox("Example Textbox (Plain)");
+let textbox2 = panel.addTextbox("Example Textbox (Money)", "€", "0.00");
+
+let dropdown = panel.addDropdown("Test Dropdown");
+dropdown.addOption(new DropdownOption("Avicia Paper", "https://raw.githubusercontent.com/JayMensink/WynnCrafter/main/sandbox/ingredient_images/Acacia%20Paper.png"));
+dropdown.addOption(new DropdownOption("Avicia Wood", "https://raw.githubusercontent.com/JayMensink/WynnCrafter/main/sandbox/ingredient_images/Acacia%20Wood.png"));
+dropdown.addOption(new DropdownOption("Molten Ingot", "https://raw.githubusercontent.com/JayMensink/WynnCrafter/main/sandbox/ingredient_images/Molten%20Ingot.png"));
+
+let button = panel.addButton("Example Button");
