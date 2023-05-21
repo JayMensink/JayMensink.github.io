e6127aed4f55c6b8c5b7543474240dd32a625759
Bitlet <j.mensink0206@gmail.com>
2023-05-21 17:16:11 +0200

    New Library

---
+++ referenceable/js/StatisticMath.js
@@ -0,0 +1,14 @@
+class StatisticMath {
+
+	constructor() { throw new Error("Class cannot be instantiated."); }
+
+	// Cumulative Distribution Function
+	static CDF(mean, sigma, to) {
+		var z = (to-mean)/Math.sqrt(2*sigma*sigma);
+		var t = 1/(1+0.3275911*Math.abs(z));
+		var erf = 1-(((((1.061405429*t + -1.453152027)*t) + 1.421413741)*t + -0.284496736)*t + 0.254829592)*t*Math.exp(-z*z);
+		var sign = (z >= 0) * 2 - 1;
+		return (1/2)*(1+sign*erf);
+	}
+	
+}
